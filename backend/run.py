103041dc747b13a146b52f2a71b6d68d5e5f6e77
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2018-03-29 02:42:30 -0400

    initial working flask app

---
+++ backend/run.py
@@ -0,0 +1,10 @@
+from flask import Flask, Response, request
+
+app = Flask('Campus Safety Indicator')
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
