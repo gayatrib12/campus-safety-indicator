d2504e248cedf0ad21b210e9410318e8bfb0299e
Kunwardeep Singh <kunwardeep.singh@gmail.com>
2018-04-15 19:25:56 -0400

    Added trends functionality

--- backend/logic/db_queries.py
+++ backend/logic/db_queries.py
@@ -8,3 +8,37 @@ institute_crime_count = """select WEAPONS + DRUGS + LIQUOR, branch
                             ) inst
                             where {crime_table}.instituteid = inst.id and year = {year}
                                 and location = '{location}'"""
+
+all_criminal_offences_year_group = "select YEAR, SUM(CRIMINAL_OFFENCES) from " \
+                                   "(select year, (MURDER + NEGLIGENT_MANSLAUGHTER + FORCIBLE_SEX+NONFORCIBLE_SEX+" \
+                                   "ROBBERY+AGGRAVATED_ASSAULTS+BURGLARY+MOTOR_VEHICLE_THEFT+ARSON) " \
+                                   "as CRIMINAL_OFFENCES from CRIMINAL C JOIN INSTITUTE I on C.INSTITUTEID = I.ID " \
+                                   "JOIN SECTOR S on I.SECTORID = S.ID {filter_str}) group by  YEAR"
+
+all_hate_offences_year_group = "select YEAR, SUM(CRIMINAL_OFFENCES) from " \
+                                   "(select year, (MURDER + FORCIBLE_SEX+NONFORCIBLE_SEX+" \
+                                   "ROBBERY+AGGRAVATED_ASSAULTS+BURGLARY+MOTOR_VEHICLE_THEFT+ARSON+VANDALISM+INTIMIDATION+" \
+                                   "SIMPLE_ASSAULT+LARCENY) " \
+                                   "as CRIMINAL_OFFENCES from HATE C JOIN INSTITUTE I on C.INSTITUTEID = I.ID " \
+                                   "JOIN SECTOR S on I.SECTORID = S.ID {filter_str}) group by  YEAR"
+
+all_vawa_offences_year_group = "select YEAR, SUM(CRIMINAL_OFFENCES) from " \
+                                   "(select year, (DOMESTIC_VIOLENCE+DATING_VIOLENCE+STALKING) " \
+                                   "as CRIMINAL_OFFENCES from VAWA C JOIN INSTITUTE I on C.INSTITUTEID = I.ID " \
+                                   "JOIN SECTOR S on I.SECTORID = S.ID {filter_str}) group by  YEAR"
+
+all_arrests_year_group = "select YEAR, SUM(CRIMINAL_OFFENCES) from " \
+                                   "(select year, (WEAPONS+DRUGS+LIQUOR) " \
+                                   "as CRIMINAL_OFFENCES from ARREST C JOIN INSTITUTE I on C.INSTITUTEID = I.ID " \
+                                   "JOIN SECTOR S on I.SECTORID = S.ID {filter_str}) group by  YEAR"
+
+all_da_year_group = "select YEAR, SUM(CRIMINAL_OFFENCES) from " \
+                                   "(select year, (WEAPONS+DRUGS+LIQUOR) " \
+                                   "as CRIMINAL_OFFENCES from DISCIPLINARY_ACTION C JOIN INSTITUTE I on C.INSTITUTEID = I.ID " \
+                                   "JOIN SECTOR S on I.SECTORID = S.ID {filter_str}) group by  YEAR"
+
+generic_year_group = "select year,sum({sub_cat_type}) from" \
+                     "(select * from {cat_type} C JOIN INSTITUTE I on C.INSTITUTEID = I.ID " \
+                     "JOIN SECTOR S on I.SECTORID = S.ID" \
+                     "{filter_str}) group by YEAR"
+
---
+++ backend/logic/trends.py
@@ -0,0 +1,113 @@
+from backend import db
+from backend.logic import db_queries
+
+
+def get_generic_trends(cat, subcat, cond):
+    print(db_queries.generic_year_group.format(
+        sub_cat_type = subcat,
+        cat_type = cat,
+        filter_str = cond
+        )
+    )
+    rows = db.cursor.execute(db_queries.generic_year_group.format(
+        sub_cat_type = subcat,
+        cat_type = cat,
+        filter_str = cond
+    )
+    ).fetchall()
+    return rows
+
+def get_all_criminal_offences_trends(cond):
+    print(db_queries.all_criminal_offences_year_group.format(
+        filter_str=cond
+    ))
+    rows = db.cursor.execute(db_queries.all_criminal_offences_year_group.format(
+        filter_str=cond
+    )).fetchall()
+    return rows
+
+def get_all_hate_trends(cond):
+    print(db_queries.all_hate_offences_year_group.format(
+        filter_str=cond
+    ))
+    rows = db.cursor.execute(db_queries.all_hate_offences_year_group.format(
+        filter_str=cond
+    )).fetchall()
+    return rows
+
+def get_all_vawa_trends(cond):
+    print(db_queries.all_vawa_offences_year_group.format(
+        filter_str=cond
+    ))
+    rows = db.cursor.execute(db_queries.all_vawa_offences_year_group.format(
+        filter_str=cond
+    )).fetchall()
+    return rows
+
+def get_all_arrest_trends(cond):
+    print(db_queries.all_criminal_offences_year_group.format(
+        filter_str=cond
+    ))
+    rows = db.cursor.execute(db_queries.all_criminal_offences_year_group.format(
+        filter_str=cond
+    )).fetchall()
+    return rows
+
+def get_all_disciplinary_action_trends(cond):
+    print(db_queries.all_da_year_group.format(
+        filter_str=cond
+    ))
+    rows = db.cursor.execute(db_queries.all_da_year_group.format(
+        filter_str=cond
+    )).fetchall()
+    return rows
+
+def generateFilterString(filter_input, form_data):
+    cond = ' where '
+    if filter_input == '0':
+        return ''
+
+    elif filter_input == 'SECTOR':
+        sectorlist = form_data.getlist(filter_input.lower())
+        cond += "S.ID in ('"
+        for item in sectorlist:
+            cond += item+"','"
+        cond = cond[:-2] + ") "
+        return cond
+
+    elif filter_input == 'LEVEL':
+        levellist = form_data.getlist(filter_input.lower())
+        sectorlist = []
+        for item in levellist:
+            sectorlist += item.split(",")
+        cond += "S.ID in ('"
+        for item in sectorlist:
+            cond += item+"','"
+        cond = cond[:-2] + ") "
+        return cond
+
+    elif filter_input == 'CONTROL':
+        controllist = form_data.getlist(filter_input.lower())
+        sectorlist = []
+        for item in controllist:
+            sectorlist += item.split(",")
+        cond += "S.ID in ('"
+        for item in sectorlist:
+            cond += item+"','"
+        cond = cond[:-2] + ") "
+        return cond
+
+    elif filter_input == 'LOCATION':
+        sectorlist = form_data.getlist(filter_input.lower())
+        cond += "C.LOCATION in ('"
+        for item in sectorlist:
+            cond += item+"','"
+        cond = cond[:-2] + ") "
+        return cond
+
+    elif filter_input == 'STATE':
+        if [form_data['state_input']][0] == 'ALL':
+            return ''
+        else:
+            return cond + filter_input + " = '" + [form_data['state_input']][0]+"'"
+
