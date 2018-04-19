974ccb4ed4d27932046c0eba0ac63dd8bb78c6de
Akshay Singh Jetawat <akshaym.jetawat@gmail.com>
2018-04-19 19:10:54 -0400

    added queries to get ranks for arrest

--- backend/logic/db_queries.py
+++ backend/logic/db_queries.py
@@ -8,3 +8,19 @@ institute_crime_count = """select WEAPONS + DRUGS + LIQUOR, branch
                             ) inst
                             where {crime_table}.instituteid = inst.id and year = {year}
                                 and location = '{location}'"""
+
+arrest_institute_rank = """select *
+from (select name, rank() over (order by arrest_count desc), arrest_count
+        from (select institute.NAME, SUM(weapons + drugs + liquor) as arrest_count
+                from arrest,institute where arrest.INSTITUTEID = institute.ID
+                group by institute.name
+            )
+    )
+where rownum <= 50"""
+
+arrest_state_rank = """select state, rank() over (order by arrest_count desc), arrest_count
+        from (select institute.STATE, SUM(weapons + drugs + liquor) as arrest_count
+                from arrest,institute where arrest.INSTITUTEID = institute.ID and state is not null
+                group by institute.STATE
+            )"""
+
