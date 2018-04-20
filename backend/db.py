52fe38a7fb368e8cc0ac3ad9a94c98aaf150bf72
Kunwardeep Singh <kunwardeep.singh@gmail.com>
2018-04-20 00:55:35 -0400

    Blank institute fixed

--- backend/db.py
+++ backend/db.py
@@ -5,6 +5,7 @@ def db_cursor():
     connection = cursor = None
     try:
         connection = cx_Oracle.connect(db_cred.username, db_cred.password, f"{db_cred.host}/{db_cred.sid}")
+        connection.current_schema = 'asj'
         cursor = connection.cursor()
         return cursor
     except Exception as e:
